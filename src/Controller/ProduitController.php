a44e9c3dd641a587a1959b857a89aa6435a17c4f
Soumaya.Marzouk <soumaya.marzouk@gmail.com>
2024-03-19 00:45:43 +0100

    rechreche

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -4,21 +4,41 @@ namespace App\Controller;
 
 use App\Entity\Produit;
 use App\Form\ProduitType;
+use App\Form\SearchType;
 use App\Repository\ProduitRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 #[Route('/produit')]
 class ProduitController extends AbstractController
 {
-    #[Route('/', name: 'app_produit_index', methods: ['GET'])]
-    public function index(ProduitRepository $produitRepository): Response
+    #[Route('/', name: 'app_produit_index', methods: ['GET','POST'])]
+    public function index(Request $request,ProduitRepository $produitRepository): Response
     {
+        $res=$produitRepository->findAll();
+
+        $defaultData = ['message' => 'Recherche par nom produit'];
+        $form = $this->createFormBuilder($defaultData)
+            ->add('nom', TextType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+            $res=$produitRepository->findBy(array('libelle' => $data['nom']));
+        }
+
+        // ... render the form
+    
+
         return $this->render('produit/index.html.twig', [
-            'produits' => $produitRepository->findAll(),
+            'produits' => $res, 'form' => $form
         ]);
     }
 
